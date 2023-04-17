7a2425538333ba479d59335620730a592471dbbc
britto <josebrittosaaji@gmail.com>
2023-04-17 01:55:53 -0400

    Created Constants class

---
+++ BrainFuckInterpreter/Constants.cs
@@ -0,0 +1,7 @@
+namespace BrainFuckInterpreter;
+
+public static class Constants
+{
+    public const string MEMORY_STRIP_LENGTH = "MEMORY_STRIP_LENGTH";
+    public const int DEFAULT_MEMORY_STRIP_LENGTH = 40000;
+}
